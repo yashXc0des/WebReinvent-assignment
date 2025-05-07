380c81b838d9c979c97d3965e3642116c03c7ef2
Alphasquare1 <yashXcodes@gmail.com>
2025-05-07 10:43:24 +0530

    build

--- android/app/src/main/kotlin/com/example/webreinvent_assignment/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.webreinvent_assignment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/core/utils/responsive.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-double responsive(BuildContext context, double size) {
-  double screenWidth = MediaQuery.of(context).size.width;
-  return size * (screenWidth / 375).clamp(0.85, 1.2);
-}
